190edf4175fdd8f459a6944027d63eeb8556c5a1
Carlos Figueira <carlosfigueirafilho@hotmail.com>
2011-11-11 07:26:43 -0800

    Service for self-hosted TCP service consumed by Silverlight.

---
+++ SilverlightTcpSelfHostedService/SLApp/MainPage.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+
+namespace SLApp
+{
+    public partial class MainPage : UserControl
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            ServiceReference1.TestClient client = new ServiceReference1.TestClient();
+            client.EchoCompleted += new EventHandler<ServiceReference1.EchoCompletedEventArgs>(client_EchoCompleted);
+            client.EchoAsync("Hello world");
+            this.AddToDebug("Called the service");
+        }
+
+        void client_EchoCompleted(object sender, ServiceReference1.EchoCompletedEventArgs e)
+        {
+            this.AddToDebug("Service replied: {0}", e.Result);
+        }
+
+        void AddToDebug(string text, params object[] args)
+        {
+            if (args != null && args.Length > 0)
+            {
+                text = string.Format(text, args);
+            }
+
+            this.txtDebug.Text = this.txtDebug.Text + text + Environment.NewLine;
+        }
+    }
+}
---
+++ SilverlightTcpSelfHostedService/Service/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.ServiceModel;
+using System.ServiceModel.Description;
+
+namespace Service
+{
+    [ServiceContract]
+    public interface ITest
+    {
+        [OperationContract]
+        string Echo(string text);
+    }
+    public class Service : ITest
+    {
+        public string Echo(string text)
+        {
+            return text;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string baseAddressTcp = "net.tcp://localhost:4504/Service";
+            ServiceHost host = new ServiceHost(typeof(Service), new Uri(baseAddressTcp));
+            host.AddServiceEndpoint(typeof(ITest), new NetTcpBinding(SecurityMode.None), "");
+            ServiceMetadataBehavior smb = new ServiceMetadataBehavior();
+            host.Description.Behaviors.Add(smb);
+            host.AddServiceEndpoint(typeof(IMetadataExchange), MetadataExchangeBindings.CreateMexTcpBinding(), "mex");
+            host.Open();
+            Console.WriteLine("Host opened");
+            Console.Write("Press ENTER to close");
+            Console.ReadLine();
+            host.Close();
+        }
+    }
+}
